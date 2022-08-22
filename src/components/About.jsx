3c93b03989134867a95660f9aff2090f933423af
antidote198 <segundojames@gmail.com>
2022-08-22 08:07:42 +0800

    added more text in #about and adjusted spacing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,27 +17,33 @@ const About = () => {
                             alt="a photo of my face"
                         />
                     </div>
-                    <div class="ml-10 mt-8">
-                        <p class="text-[#8892b0] text-[24px] about-me-text">
+                    <div class="ml-10 mt-7">
+                        <p class="text-[#8892b0] text-[21px] about-me-text">
                             Hello, my name is
                             <span class="text-secondary"> James Segundo</span>,
                             an aspiring web developer with a degree in
                             <span class="text-secondary">
                                 &nbsp;Computer Engineering
                             </span>
-                            . I have a good background in programming languages
-                            and tools such as
+                            .<br />I have a good background in programming
+                            languages and tools such as
                             <span class="text-secondary">&nbsp;C</span>,
                             <span class="text-secondary">&nbsp;C#</span>,
                             <span class="text-secondary">&nbsp;Python</span>,
                             <span class="text-secondary">&nbsp;Java</span>,
                             <span class="text-secondary">&nbsp;HTML</span>,
                             <span class="text-secondary">&nbsp;CSS</span>,
-                            <span class="text-secondary">&nbsp;Bootstrap</span>,
                             <span class="text-secondary">&nbsp;JavaScript</span>
-                            , and
-                            <span class="text-secondary">&nbsp;SQL</span>. I
-                            also acquire basic knowledge about
+                            ,<span class="text-secondary">&nbsp;Bootstrap</span>
+                            ,
+                            <span class="text-secondary">
+                                &nbsp;Tailwind CSS
+                            </span>
+                            ,<span class="text-secondary">&nbsp;ReactJS</span>,
+                            <span class="text-secondary">&nbsp;ExpressJS</span>,
+                            and
+                            <span class="text-secondary">&nbsp;SQL</span>.<br />
+                            I also acquire basic knowledge about
                             <span class="text-secondary">
                                 &nbsp;Object-Oriented Programming
                             </span>
